95bccaf4203f496a1e5d24e8f4989dac76440407
Hugo Veiga <hugoveiga10@gmail.com>
2023-11-29 21:58:27 +0100

    Add test suite for NavBar component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,11 @@
+import NavBar from "../NavBar/NavBar";
 import HeaderStyled from "./HeaderStyled";
 
 const Header = (): React.ReactElement => {
   return (
     <HeaderStyled className="header">
       <h1 className="header__title">Inkventory</h1>
+      <NavBar />
     </HeaderStyled>
   );
 };
