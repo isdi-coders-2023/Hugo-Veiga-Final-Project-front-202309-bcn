78dbcfffe15a2a147d519be12aac247282ebeba9
Hugo Veiga <hugoveiga10@gmail.com>
2023-11-29 13:00:58 +0100

    Add GlobalStyle

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import GlobalStyle from "./styles/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import mainTheme from "./styles/mainTheme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div />
-  </React.StrictMode>
+    <ThemeProvider theme={mainTheme}>
+      <GlobalStyle />
+    </ThemeProvider>
+  </React.StrictMode>,
 );
---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,57 @@
+import { createGlobalStyle } from "styled-components";
+import "@fontsource-variable/jura";
+
+const GlobalStyle = createGlobalStyle`
+ 
+ *,
+::after,
+::before {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: ${({ theme }) => theme.typography.mainFont};
+}
+
+h1,
+h2,
+body {
+  margin: 0;
+  padding: 0;
+}
+
+p {
+  margin: 0;
+}
+
+img { 
+  width: 100%;
+}
+
+a{
+text-decoration: none;
+color: inherit;
+}
+
+ul,
+li {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+input, textarea, select{
+  border: none;
+  font-family: inherit;
+  outline: none
+}
+
+button{
+  border: none;
+  font: inherit;
+  cursor: pointer;
+}
+
+`;
+
+export default GlobalStyle;
