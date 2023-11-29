feb0310fb4166279da3c78f7f76d5fa90d267754
Hugo Veiga <hugoveiga10@gmail.com>
2023-11-29 13:24:52 +0100

    Add App component

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import GlobalStyle from "./styles/GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import mainTheme from "./styles/mainTheme";
+import App from "./components/App/App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={mainTheme}>
       <GlobalStyle />
+      <App />
     </ThemeProvider>
   </React.StrictMode>,
 );
