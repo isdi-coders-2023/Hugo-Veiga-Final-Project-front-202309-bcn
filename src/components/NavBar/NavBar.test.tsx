9653e6e27bf1af2861e413e8db69834cf84ab0ae
Hugo Veiga <hugoveiga10@gmail.com>
2023-11-30 10:12:49 +0100

    Add new test case to NavBar test

--- src/components/NavBar/NavBar.test.tsx
+++ src/components/NavBar/NavBar.test.tsx
@@ -9,6 +9,7 @@ describe("Given a NavBar component", () => {
     test("Then it should show the text `Tattoos`", () => {
       const expectedText = "Tattoos";
       const expectedAccesibelName = "to tattoos";
+
       render(
         <BrowserRouter>
           <ThemeProvider theme={mainTheme}>
@@ -20,9 +21,11 @@ describe("Given a NavBar component", () => {
       const tattoosNavLink = screen.getByRole("link", {
         name: expectedAccesibelName,
       });
+
       expect(tattoosNavLink).toHaveTextContent(expectedText);
     });
   });
+
   test("Then it should be a link with the path `/tattoos`", () => {
     const expectedPath = "/tattoos";
     const expectedAccesibleName = "to tattoos";
@@ -38,6 +41,23 @@ describe("Given a NavBar component", () => {
     const tattoosNavLink = screen.getByRole("link", {
       name: expectedAccesibleName,
     });
+
     expect(tattoosNavLink).toHaveAttribute("href", expectedPath);
+  });
+
+  test("Then it should show 2 links", async () => {
+    const expectedLinkAmount = 2;
+
+    render(
+      <BrowserRouter>
+        <ThemeProvider theme={mainTheme}>
+          <Navigation />
+        </ThemeProvider>
+      </BrowserRouter>,
+    );
+
+    const tattoosNavLink = await screen.findAllByRole("link");
+
+    expect(tattoosNavLink).toHaveLength(expectedLinkAmount);
   });
 });
