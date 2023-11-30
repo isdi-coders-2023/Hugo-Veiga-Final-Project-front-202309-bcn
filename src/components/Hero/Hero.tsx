397b8d23054a957dc2193c898ba8a1bde5158177
Hugo Veiga <hugoveiga10@gmail.com>
2023-11-30 19:52:29 +0100

    Add Header and Routes to App component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,16 +2,14 @@ import HeroStyled from "./HeroStyled";
 
 const Hero = (): React.ReactElement => {
   return (
-    <>
-      <HeroStyled className="hero">
-        <div
-          className="hero__image"
-          aria-label="ink texture hero background image"
-        >
-          <span className="hero__text">Your own tattoo ideas</span>
-        </div>
-      </HeroStyled>
-    </>
+    <HeroStyled className="hero">
+      <div
+        className="hero__image"
+        aria-label="ink texture hero background image"
+      >
+        <span className="hero__text">Your own tattoo ideas</span>
+      </div>
+    </HeroStyled>
   );
 };
 export default Hero;
