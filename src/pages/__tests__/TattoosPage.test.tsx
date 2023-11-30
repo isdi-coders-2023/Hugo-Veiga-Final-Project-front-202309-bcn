bb8c840b62f8e90a2683b4eee7b1aef1bec17104
Hugo Veiga <hugoveiga10@gmail.com>
2023-11-30 19:46:51 +0100

    Add TattoosPage test suite

---
+++ src/pages/__tests__/TattoosPage.test.tsx
@@ -0,0 +1,19 @@
+import { screen } from "@testing-library/react";
+import customRenderProvider from "../../testUtils/customRenderProvider";
+import TattoosPage from "../TattoosPage";
+
+describe("Given a TattoosPage component", () => {
+  describe("When is rendered", () => {
+    test("Then it should show a heading containing the text `Tattoo List`", () => {
+      const expectedHeadingText = "Tattoo List";
+
+      customRenderProvider(<TattoosPage />);
+
+      const headingText = screen.getByRole("heading", {
+        name: expectedHeadingText,
+      });
+
+      expect(headingText).toBeInTheDocument();
+    });
+  });
+});
