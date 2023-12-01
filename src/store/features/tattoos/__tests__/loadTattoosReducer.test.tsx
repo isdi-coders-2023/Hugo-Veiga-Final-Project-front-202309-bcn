43f0f990d4bf67f9a26dc0552ce8c7ef876b0afa
Hugo Veiga <hugoveiga10@gmail.com>
2023-12-01 12:09:25 +0100

    Add laodTattoosReducer test

---
+++ src/store/features/tattoos/__tests__/loadTattoosReducer.test.tsx
@@ -0,0 +1,29 @@
+import tattoosMock from "../../../../mocks/tattoosMocks";
+import { loadTattoosActionCreator, tattoosReducer } from "../tattoosSlice";
+import { TattoosStateStructure, TattooStructure } from "../types";
+
+describe("Given a tattoosSlice reducer", () => {
+  describe("When it receives an empty tattooState and an action to load tattooos", () => {
+    test("Then it should return a new state with tattoos data on it", () => {
+      const emptyState: TattooStructure[] = [];
+
+      const currentTattoosState: TattoosStateStructure = {
+        tattoos: emptyState,
+      };
+
+      const loadTattoosAction = loadTattoosActionCreator(tattoosMock);
+
+      const expectedNewTattoosState: TattoosStateStructure = {
+        ...currentTattoosState,
+        tattoos: tattoosMock,
+      };
+
+      const newState: TattoosStateStructure = tattoosReducer(
+        currentTattoosState,
+        loadTattoosAction,
+      );
+
+      expect(expectedNewTattoosState).toStrictEqual(newState);
+    });
+  });
+});
