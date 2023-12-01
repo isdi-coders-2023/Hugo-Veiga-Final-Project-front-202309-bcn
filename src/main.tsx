b626fb4e2b5bdd076d0e1d98f1e24be9fbade7ba
Hugo Veiga <hugoveiga10@gmail.com>
2023-12-01 12:14:35 +0100

    Dispatch loadTattoos on TattoosPage

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,18 @@ import GlobalStyle from "./styles/GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import mainTheme from "./styles/mainTheme";
 import App from "./components/App/App";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider theme={mainTheme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider theme={mainTheme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 );
--- src/pages/TattoosPage.tsx
+++ src/pages/TattoosPage.tsx
@@ -1,11 +1,27 @@
+import { useDispatch } from "react-redux";
 import Hero from "../components/Hero/Hero";
+import { useAppSelector } from "../store/hooks";
 import TattoosPageStyled from "./TattoosPageStyled";
+import tattoosMock from "../mocks/tattoosMocks";
+import { useEffect } from "react";
+import { loadTattoosActionCreator } from "../store/features/tattoos/tattoosSlice";
 
 const TattoosPage = (): React.ReactElement => {
+  const tattooState = useAppSelector((tatoo) => tatoo.tattoosState);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadTattoosActionCreator(tattoosMock));
+  }, [dispatch]);
+
   return (
     <TattoosPageStyled className="tattoos-page">
       <Hero />
       <h1 className="tattoos-page__title">Tattoo List</h1>
+      {tattooState.tattoos.map((tattoo) => (
+        <span key={tattoo.id}>{tattoo.artist}</span>
+      ))}
     </TattoosPageStyled>
   );
 };
