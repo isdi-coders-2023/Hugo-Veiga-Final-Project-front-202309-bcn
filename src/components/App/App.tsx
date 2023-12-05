58c25f74e14e87c91a4617d7e927fb0484c0c273
Hugo Veiga <hugoveiga10@gmail.com>
2023-12-05 16:34:02 +0100

    Update LoadingStyled

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,15 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Header from "../Header/Header";
 import TattoosPage from "../../pages/TattoosPage/TattoosPage";
+import Loading from "../Loading/Loading";
+import { useAppSelector } from "../../store/hooks";
 
 const App = (): React.ReactElement => {
+  const uiState = useAppSelector((state) => state.uiState);
+
   return (
     <>
+      {uiState.isLoading && <Loading />}
       <Header />
       <Routes>
         <Route path="/" element={<Navigate to="/tattoos" />} />
--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -4,7 +4,7 @@ const Loading = (): React.ReactElement => {
   return (
     <LoadingStyled className="loading">
       <span className="laoding__spinner"></span>
-      <span className="loading__text">Loading...</span>
+      <span className="loading__text">Loading ...</span>
     </LoadingStyled>
   );
 };
