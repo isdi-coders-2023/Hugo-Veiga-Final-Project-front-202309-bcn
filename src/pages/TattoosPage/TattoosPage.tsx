057b31cdfc9e527a31bf27d134ac888531723b9f
Hugo Veiga <hugoveiga10@gmail.com>
2023-12-05 10:46:35 +0100

    Dispatch loadTattoos on TattoosPage

--- src/pages/TattoosPage/TattoosPage.tsx
+++ src/pages/TattoosPage/TattoosPage.tsx
@@ -1,15 +1,28 @@
+import { useDispatch } from "react-redux";
 import Hero from "../../components/Hero/Hero";
 import TattooList from "../../components/TattooList/TattooList";
-import tattoosMock from "../../data/tattoosData";
+import useTattoosApi from "../../hooks/useTattoosApi";
 import TattoosPageStyled from "./TattoosPageStyled";
+import { useEffect } from "react";
+import { loadTattoosActionCreator } from "../../store/features/tattoos/tattoosSlice";
 
 const TattoosPage = (): React.ReactElement => {
+  const dispatch = useDispatch();
+  const { getTattoos } = useTattoosApi();
+
+  useEffect(() => {
+    (async () => {
+      const { tattoos } = await getTattoos();
+
+      dispatch(loadTattoosActionCreator(tattoos));
+    })();
+  }, [dispatch, getTattoos]);
   return (
     <>
       <Hero />
       <TattoosPageStyled className="tattoos-page">
         <h1 className="tattoos-page__title">Tattoo List</h1>
-        <TattooList tattoos={tattoosMock} />
+        <TattooList />
       </TattoosPageStyled>
     </>
   );
