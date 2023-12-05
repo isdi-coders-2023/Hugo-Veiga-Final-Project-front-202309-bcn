0629347406bbb5c01e7825074f24f8b532cba2dc
Hugo Veiga <hugoveiga10@gmail.com>
2023-12-05 12:15:43 +0100

    Add Loading component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,12 @@
+import LoadingStyled from "./LoadingStyled";
+
+const Loading = (): React.ReactElement => {
+  return (
+    <LoadingStyled className="loading">
+      <span className="laoding__spinner"></span>
+      <span className="loading__text">Loading...</span>
+    </LoadingStyled>
+  );
+};
+
+export default Loading;
