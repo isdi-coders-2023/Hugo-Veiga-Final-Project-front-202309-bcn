389b51cca92f176f2f09e38e95207f1d25bb4c4f
Hugo Veiga <hugoveiga10@gmail.com>
2023-12-06 18:07:28 +0100

    Add test case for error loading tattoos from api

--- src/pages/TattoosPage/TattoosPage.test.tsx
+++ src/pages/TattoosPage/TattoosPage.test.tsx
@@ -1,6 +1,8 @@
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 import customRenderProvider from "../../testUtils/customRenderProvider";
 import TattoosPage from "./TattoosPage";
+import { server } from "../../mocks/msw/node";
+import handlersError from "../../mocks/msw/handlersError";
 
 describe("Given a TattoosPage component", () => {
   describe("When is rendered", () => {
@@ -14,6 +16,19 @@ describe("Given a TattoosPage component", () => {
       });
 
       expect(headingText).toBeInTheDocument();
+    });
+  });
+
+  describe("When TattooPage is rendered and there is an error loading tattoos", () => {
+    test("Then it should call toastify with message the'Error loading tattoos'", async () => {
+      const expectedTextError = "Error loading tattoos";
+      server.use(...handlersError);
+
+      customRenderProvider(<TattoosPage />);
+
+      await waitFor(() => {
+        expect(screen.getByText(expectedTextError)).toBeInTheDocument();
+      });
     });
   });
 });
