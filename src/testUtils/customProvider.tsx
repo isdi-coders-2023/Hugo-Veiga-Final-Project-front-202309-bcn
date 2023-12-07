82a5db3e392d36fc94fdb81da4c59f012309765c
Hugo Veiga <hugoveiga10@gmail.com>
2023-12-07 12:53:10 +0100

    Fix deleteTattoo useTattoosApi's method test

--- src/testUtils/customProvider.tsx
+++ src/testUtils/customProvider.tsx
@@ -1,17 +1,12 @@
 import { PropsWithChildren } from "react";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
 import { store } from "../store";
 import { tattoosReducer } from "../store/features/tattoos/tattoosSlice";
 import tattoosMock from "../mocks/tattoosMocks";
 import { configureStore } from "@reduxjs/toolkit";
 
 export const customProvider = ({ children }: PropsWithChildren) => {
-  return (
-    <BrowserRouter>
-      <Provider store={store}>{children}</Provider>
-    </BrowserRouter>
-  );
+  return <Provider store={store}>{children}</Provider>;
 };
 
 export const getMockedStore = () => {
