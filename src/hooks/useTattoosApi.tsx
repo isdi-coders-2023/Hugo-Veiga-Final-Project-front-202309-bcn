7556d37f058b05f3ccaf77c7d6c0759d5be31151
Hugo Veiga <hugoveiga10@gmail.com>
2023-12-07 12:29:50 +0100

    Add deleteTattoo method to useTattoosApi hook

--- src/hooks/useTattoosApi.tsx
+++ src/hooks/useTattoosApi.tsx
@@ -1,22 +1,19 @@
 import { useCallback } from "react";
 import axios from "axios";
-import {
-  TattooStructure,
-  TattoosStateStructure,
-} from "../store/features/tattoos/types";
-import { UseTattoosApiStructure } from "./types";
+import { toast } from "react-toastify";
+import { TattooStructure } from "../store/features/tattoos/types";
 import { useAppDispatch } from "../store/hooks";
 import {
   hideLoadingActionCreator,
   showLoadingActionCreator,
 } from "../store/features/ui/uiSlice";
 
-const useTattoosApi = (): UseTattoosApiStructure => {
+const useTattoosApi = () => {
   const dispatch = useAppDispatch();
 
   axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
-  const getTattoos = useCallback(async (): Promise<TattoosStateStructure> => {
+  const getTattoos = useCallback(async () => {
     dispatch(showLoadingActionCreator());
     try {
       const { data: tattoos } = await axios.get<{
@@ -33,7 +30,45 @@ const useTattoosApi = (): UseTattoosApiStructure => {
     }
   }, [dispatch]);
 
-  return { getTattoos };
+  const deleteTattoo = useCallback(
+    async (tattooId: string) => {
+      dispatch(showLoadingActionCreator());
+      try {
+        const { data } = await axios.delete(`/tattoos/delete/${tattooId}`);
+
+        dispatch(hideLoadingActionCreator());
+
+        toast.success("Tattoo deleted succesfully!", {
+          position: "top-center",
+          autoClose: 2000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        });
+
+        return data;
+      } catch {
+        dispatch(hideLoadingActionCreator());
+
+        toast.error("There was an error deleting the tattoo", {
+          position: "top-center",
+          autoClose: 2000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        });
+      }
+    },
+    [dispatch],
+  );
+
+  return { getTattoos, deleteTattoo };
 };
 
 export default useTattoosApi;
