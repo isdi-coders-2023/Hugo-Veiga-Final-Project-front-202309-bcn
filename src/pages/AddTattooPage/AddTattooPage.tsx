83d7c0d85711b25960c9a2740d4e75678aeb81d1
Hugo Veiga <hugoveiga10@gmail.com>
2023-12-11 09:41:36 +0100

    Add navigate to TattoosPage and scroll top when a tattoo is created

--- src/pages/AddTattooPage/AddTattooPage.tsx
+++ src/pages/AddTattooPage/AddTattooPage.tsx
@@ -1,27 +1,15 @@
 import TattooForm from "../../components/TattooForm/TattooForm";
-import { useAppDispatch } from "../../store/hooks";
 import AddTattooPageStyled from "./AddTattooPageStyled";
 import useTattoosApi from "../../hooks/useTattoosApi";
-import { TattooStructureWithoutId } from "../../store/features/tattoos/types";
-import { addTattooActionCreator } from "../../store/features/tattoos/tattoosSlice";
 
 const AddTattooPage = (): React.ReactElement => {
   const { addTattoo } = useTattoosApi();
-  const dispatch = useAppDispatch();
-
-  const onSubmit = async (tattooData: TattooStructureWithoutId) => {
-    const newTattoo = await addTattoo(tattooData);
-
-    if (newTattoo) {
-      dispatch(addTattooActionCreator(newTattoo));
-    }
-  };
 
   return (
     <>
       <AddTattooPageStyled className="addTattoo-page">
         <h1 className="addTattoo-page__title">Add a new tattoo</h1>
-        <TattooForm onSubmit={onSubmit} />
+        <TattooForm onSubmit={addTattoo} />
       </AddTattooPageStyled>
     </>
   );
--- src/pages/TattoosPage/TattoosPage.tsx
+++ src/pages/TattoosPage/TattoosPage.tsx
@@ -34,6 +34,7 @@ const TattoosPage = (): React.ReactElement => {
       }
     })();
   }, [dispatch, getTattoos]);
+
   return (
     <>
       <Hero />
