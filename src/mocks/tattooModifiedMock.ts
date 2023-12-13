a61509484e0cde2deaa37114db38b0164aa072f7
Hugo Veiga <hugoveiga10@gmail.com>
2023-12-13 21:52:36 +0100

    Add tattooModifiedMock

---
+++ src/mocks/tattooModifiedMock.ts
@@ -0,0 +1,15 @@
+const tattooModifiedMock = {
+  _id: "6571d83d81f419ec2f6fc543",
+  artist: "MissSita",
+  email: "hello.misssita@hotmail.com",
+  instagram: "https://www.instagram.com/misssita",
+  city: "Barcelona",
+  style: "blackwork",
+  image: "https://i.ibb.co/W2DBhvK/miss-Sita-tattoo-1.webp",
+  notes: "Ask to review the fan tattoo ",
+  isFavorite: true,
+  direction:
+    "https://www.google.com/maps/dir//109+tattoo+barcelona/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x12a4a2fa4f62e835:0xede092fc03c85d0?sa=X&ved=2ahUKEwjZ7OSj3OSCAxW2TKQEHaiVDSwQ9Rd6BAhAEAA",
+};
+
+export default tattooModifiedMock;
